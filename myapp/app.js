c1d3c31cc80b12891146cb7f502df50016d5e2d3
bgmoshe <barakgross5@gmail.com>
2018-05-04 17:50:42 +0300

    Fixing some error handling and input handling. Ran test, checked results, everything seems logical. Still needs to do output validation

--- myapp/app.js
+++ myapp/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var fs = require('fs');
 var app = express();
 
-entity_dict = {'1':{lat:1,lon:2}};
+entity_dict = {};
 KM_TO_NM = 0.539956803;
 
 // view engine setup
@@ -20,36 +20,51 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-function computeDistance(obj1, obj2){
-   var obj1_lat = 1.0*obj1['lat'];
-   var obj1_lon = 1.0*obj1['lon'];
+function computeDistance(entity1, entity2){
+   var entity1_latitude = 1.0*entity1['latitude'];
+   var entity1_longitude = 1.0*entity1['longitude'];
 
-   var obj2_lat = 1.0*obj2['lat'];
-   var obj2_lon = 1.0*obj2['lon'];
+   var entity2_latitude = 1.0*entity2['latitude'];
+   var entity2_longitude = 1.0*entity2['longitude'];
    
    var R = 6371e3; // metres
-   var delta_lat = obj2_lat-obj1_lat;
-   var delta_lon = obj2_lon-obj1_lon;
+   var delta_latitude = entity2_latitude-entity1_latitude;
+   var delta_longitude = entity2_longitude-entity1_longitude;
 
-   var a = Math.sin(delta_lat/2) * Math.sin(delta_lat/2) +
-      Math.cos(obj1_lat) * Math.cos(obj2_lat) *
-      Math.sin(delta_lon/2) * Math.sin(delta_lon/2);
+   var a = Math.sin(delta_latitude/2) * Math.sin(delta_latitude/2) +
+      Math.cos(entity1_latitude) * Math.cos(entity2_latitude) *
+      Math.sin(delta_longitude/2) * Math.sin(delta_longitude/2);
    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
    var d = (R * c /1000.0)*KM_TO_NM;
    return d;
 }
 
-function computeBearing(obj1, obj2){
-   var obj1_lat = 1.0*obj1['lat'];
-   var obj1_lon = 1.0*obj1['lon'];
+function getEntitiesInRange(obj, entities, page_size){
+   var counter = 0;
+   var entities_in_range = {};
+   for (var key in entities){
+      if(counter == page_size){
+	     break;
+      }
+      if(computeDistance(obj, entity_dict[key])<=obj['radius']*1.0){
+         counter++;
+         entities_in_range[key] = entity_dict[key];
+      }
+   }
+   return entities_in_range;
+}
+
+function computeBearing(entity1, entity2){
+   var entity1_latitude = 1.0*entity1['latitude'];
+   var entity1_longitude = 1.0*entity1['longitude'];
 	
-   var obj2_lat = 1.0*obj2['lat'];
-   var obj2_lon = 1.0*obj2['lon'];
+   var entity2_latitude = 1.0*entity2['latitude'];
+   var entity2_longitude = 1.0*entity2['longitude'];
    
-   var y = Math.sin(obj1_lat-obj2_lat) * Math.cos(obj1_lon);
-   var x = Math.cos(obj2_lon)*Math.sin(obj1_lon) -
-           Math.sin(obj1_lat)*Math.cos(obj1_lon)*Math.cos(obj1_lat-obj2_lat);
+   var y = Math.sin(entity2_longitude-entity1_longitude) * Math.cos(entity2_latitude);
+   var x = Math.cos(entity1_latitude)*Math.sin(entity2_latitude) -
+           Math.sin(entity1_latitude)*Math.cos(entity2_latitude)*Math.cos(entity2_longitude-entity1_longitude);
    var brng = Math.atan2(y, x);
    return brng;
 }
@@ -62,7 +77,6 @@ function findClosestID(entity, dict){
    for (var key in dict){
       var dist = computeDistance(dict[key], entity);
 	  if(dist < min_dist){
-         console.log('ID:'+key +", dist "+dist.toString()+", Closest_ID = " +closest_entity_id+" ,min_dist"+min_dist.toString()); 		 
          closest_entity_id = key;
          min_dist = dist;
       }
@@ -76,7 +90,9 @@ app.get('/entities', function (req, res) {
 
 app.get('/entity/:id', function (req, res) {
    if(req.params.id in entity_dict){
-      res.status(201).end(JSON.stringify(entity_dict[req.params.id]));
+	  var entity = {}
+	  entity[req.params.id] = entity_dict[req.params.id];
+      res.status(201).end(JSON.stringify(entity));
    }
    else{
       res.status(404).end("No such ID exists. Try another one!");
@@ -87,15 +103,15 @@ app.get('/entity/:id', function (req, res) {
 app.post('/entity', function (req, res) {
    var id = req.body.id;
    if(id in entity_dict ){
-      res.status(400).end("Invalid request. ID already exitsts");
+      res.status(400).end("Invalid request. ID already exists");
    }
    else{
-      if(!('lat' in req.body && 'lon' in req.body && 'alt' in req.body)){
+      if(!('latitude' in req.body && 'longitude' in req.body && 'altitude' in req.body)){
          res.status(400).end("Invalid request. Must contain at least a longitude, latitude and altitude fields!");
      }
       else{
-		 if(isNaN(req.body.lat) || isNaN(req.body.lon)){
-            res.status(400).end("Invalid request. Lat/Lon/Alt parameters must be integers!");
+		 if(isNaN(req.body.latitude) || isNaN(req.body.longitude) || isNaN(req.body.altitude)){
+            res.status(400).end("Invalid request. latitude/longitude/altitude parameters must be numbers!");
 		 }
 		 else{
             delete(req.body.id);
@@ -110,28 +126,16 @@ app.post('/entity', function (req, res) {
 app.get('/entities-around', function (req, res) {
 	
    var params = req.body;
-   var counter = 0;
    var MAX_PER_PAGE = 50;
-   var list = [];
+   var entities_in_range_dict = {};
 
-   if(!('lat' in params && 'lon' in params && 'radius' in params) || isNaN(params['lat'])||isNaN(params['lon'])||isNaN(params['radius'])){
-	   res.status(400).end("Invalid input. Needs integer lat,lon and radius");
+   if(!('latitude' in params && 'longitude' in params && 'radius' in params) || isNaN(params['latitude'])||isNaN(params['longitude'])||(isNaN(params['radius'])||parseFloat(params['radius'])<0)){
+	   res.status(400).end("Invalid input. Latitude and longitude must be number, while radius should be a non-negative number");
    }
    else{
-      for (var key in entity_dict){
-         if(counter == MAX_PER_PAGE){
-	        break;
-         }
-         if(computeDistance(params, entity_dict[key])<=params['radius']*1.0){
-		     counter++;
-		     list.push(key);
-         }
-		 else{
-		    console.log("Radius:"+params['radius']+" but distance is "+computeDistance(params, entity_dict[key]).toString());
-		 }
-      }
+	  entities_in_range_dict = getEntitiesInRange(params, entity_dict, MAX_PER_PAGE);  
    }
-   res.end(JSON.stringify(list))
+   res.end(JSON.stringify(entities_in_range_dict))
 });
 
 app.get('/entities-in-bearing', function (req, res) {
@@ -139,30 +143,43 @@ app.get('/entities-in-bearing', function (req, res) {
    var params = req.body;
    var counter = 0;
    var MAX_PER_PAGE = 50;
-   var list = [];
+   var entities_in_range_dict ={};
 
-   if(!('lat' in params && 'lon' in params && 'radius' in params && 'minBearing' in params && 'maxBearing' in params)
-	   || isNaN(params['lat'])||isNaN(params['lon'])||isNaN(params['radius'])||isNaN(params['minBearing'])||isNaN(params['maxBearing'])){
-	   res.status(400).end("Invalid input. Needs integer lat,lon, radius min and max bearing");
+   if(!('latitude' in params && 'longitude' in params && 'radius' in params && 'minBearing' in params && 'maxBearing' in params)
+	   || isNaN(params['latitude'])||isNaN(params['longitude'])||isNaN(params['radius'])||isNaN(params['minBearing'])||isNaN(params['maxBearing'])){
+	   res.status(400).end("Invalid input. Latitude,longitude, radius min and max bearing must be numbers");
    }
    else{
-      for (var key in entity_dict){
-         if(counter == MAX_PER_PAGE){
-	        break;
+	  if(Math.abs(params['minBearing']*1.0 - params['maxBearing']*1.0) >= 2*Math.PI){
+         entities_in_range_dict = getEntitiesInRange(params, entity_dict, MAX_PER_PAGE);
+      }
+      else{
+		 var min_bearing = params['minBearing'] * 1.0;
+		 var max_bearing = params['maxBearing'] * 1.0;
+		 
+         for (var key in entity_dict){
+            if(counter == MAX_PER_PAGE){
+	           break;
+            }
+            if(computeDistance(params, entity_dict[key])<params['radius']*1.0){
+		       var brng = computeBearing(params ,entity_dict[key])
+			   if(min_bearing <= max_bearing){
+			      if(brng >= min_bearing  && brng <= max_bearing){
+		             counter++;
+		             entities_in_range_dict[key] = entity_dict[key];
+		          }
+               }
+			   else{
+			      if(brng <= min_bearing  || brng <= max_bearing){
+		             counter++;
+		             entities_in_range_dict[key] = entity_dict[key];
+                  }				  
+			   }
+			}
          }
-         if(computeDistance(params, entity_dict[key])<params['radius']*1.0){
-		    var brng = computeBearing(params ,entity_dict[key])
-			if(brng >= params['minBearing'] && brng <= params['maxBearing']){
-		       counter++;
-		       list.push(key);
-		    }
-         }
-		 else{
-		    console.log("Radius:"+params['radius']+" but distance is "+computeDistance(params, entity_dict[key]).toString());
-		 }
       }
    }
-   res.end(JSON.stringify(list))
+   res.end(JSON.stringify(entities_in_range_dict));
 });
 
 app.get('/closest-entity', function (req, res) {
@@ -170,31 +187,36 @@ app.get('/closest-entity', function (req, res) {
    var params = req.body;
    var counter = 0;
    
-   if(!(('lat' in params && 'lon' in params) || 'id' in params)){
-      res.status(400).end("Invalid input. Needs integer lat,lon or entity id");
+   if(!(('latitude' in params && 'longitude' in params) || 'id' in params)){
+      res.status(400).end("Invalid input. Must have latitude and longitude fields or id field");
    }
    else{
-      if('lat' in params && 'lon' in params){
-	     if( isNaN(params['lat'])||isNaN(params['lon'])){
-	        res.status(400).end("Invalid. Lat and Lon should be integers");
+      if('latitude' in params && 'longitude' in params){
+         if('id' in params){
+		     res.status(400).end("Ambiguous input. Must have latitude and longitude fields or id field, not both");
 		 }
-		 else{
-            var closest_entity_id = findClosestID(params,entity_dict);
-            console.log("Parms = "+JSON.stringify(params)+" \n Closest Entity id:"+closest_entity_id.toString()+
-			            "\nEntity="+JSON.stringify(entity_dict[closest_entity_id]));
-            res.end(JSON.stringify(entity_dict[closest_entity_id]));
+	     else{
+	        if( isNaN(params['latitude'])||isNaN(params['longitude'])){
+	           res.status(400).end("Invalid. latitude and longitude should be numbers");
+			}
+		 
+		    else{
+               var closest_entity_id = findClosestID(params,entity_dict);
+			   var entity={};
+			   entity[closest_entity_id] = entity_dict[closest_entity_id]
+               res.end(JSON.stringify(entity));
+			}
 		 }
       }
 	  else{
 	     if(params['id'] in entity_dict){
-		    var entity = entity_dict[params['id']];
+		    var input_entity = entity_dict[params['id']];
             delete(entity_dict[params['id']]);
-			var closest_entity_id = findClosestID(entity,entity_dict);
-			entity[params['id']] = entity;
-			entity_dict[params['id']] = entity;
-            console.log("Parms = "+JSON.stringify(params)+" \n Closest Entity id:"+closest_entity_id.toString()+
-			"\nEntity="+JSON.stringify(entity_dict[closest_entity_id]));
-            res.end(JSON.stringify(entity_dict[closest_entity_id]));			
+			var closest_entity_id = findClosestID(input_entity,entity_dict);
+			var closest_entity = {};
+			closest_entity[closest_entity_id] = entity_dict[closest_entity_id];
+			entity_dict[params['id']] = input_entity;
+            res.end(JSON.stringify(closest_entity));			
 		 }
 		 else{
 		    res.status(400).end("Invalid. No entity has id "+params['id']);
@@ -205,7 +227,6 @@ app.get('/closest-entity', function (req, res) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-   console.log(req);
    next(createError(404));
 });
 

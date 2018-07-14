35c6b541e9347502f3e47a434b7a196348568f52
bgmoshe <barakgross5@gmail.com>
2018-07-14 23:07:45 +0300

    Fixed a forgotten WT comment

--- myapp/basic-functions.js
+++ myapp/basic-functions.js
@@ -5,7 +5,7 @@
    @description This function checks whether input.field exists and if it is a number.
    If it is, the function return true.
 */
-var containsFieldAsNumber = function containsFieldAsNumber(input, field){
+function containsFieldAsNumber(input, field){
     return field in input && !(isNaN(input[field]));
 }
 
